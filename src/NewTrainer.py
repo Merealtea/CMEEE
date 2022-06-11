1b029de47a4dadc591c7c0ede7728866a322a9b1
Merealtea <67770232+Merealtea@users.noreply.github.com>
2022-06-11 18:07:10 +0800

    update NewTrainer

--- src/NewTrainer.py
+++ src/NewTrainer.py
@@ -2,6 +2,9 @@ from typing_extensions import runtime
 from transformers import Trainer
 from torch import nn
 import torch
+from torch.optim.swa_utils import AveragedModel, SWALR
+from torch.utils.data import DataLoader,IterableDataset
+from torch.utils.data.distributed import DistributedSampler
 
 def get_parameter_names(model, forbidden_layer_types):
     """
@@ -31,7 +34,9 @@ class Trainer_lr_decay(Trainer):
         compute_metrics = None,
         callbacks  = None,
         optimizers = (None, None),
-        lr_decay_rate = None) :
+        lr_decay_rate = None,
+        swa = False,
+        swa_update_epoches = 50) :
         super(Trainer_lr_decay, self).__init__(model,args ,
                                                 data_collator,
                                                 train_dataset,
@@ -43,9 +48,25 @@ class Trainer_lr_decay(Trainer):
                                                 optimizers)
         # if Trainer.__version__ != '4.11.0':
         #     raise RuntimeError(f"要使用Trainer版本为4.11.0,当前版本为{Trainer.__version__}")
-        self.lr_decay_rate = lr_decay_rate
-        """丑是丑了点，能用就行
+
+        self.lr_decay_rate = lr_decay_rate # 假如decay_rate为-1，则就不是用lr_layer_size_decay
+
+        self.swa = swa # 如果是True，则使用参数平均
+        self.swa_update_epoches = swa_update_epoches # 参数平均的训练轮数
+        self.swa_scheduler = None
+        self.swa_model = None
+
+    def create_optimizer_and_scheduler(self, num_training_steps: int):
         """
+        Setup the optimizer and the learning rate scheduler.
+
+        We provide a reasonable default that works well. If you want to use something else, you can pass a tuple in the
+        Trainer's init through :obj:`optimizers`, or subclass and override this method (or :obj:`create_optimizer`
+        and/or :obj:`create_scheduler`) in a subclass.
+        """
+        self.create_optimizer()
+        self.create_scheduler(num_training_steps=num_training_steps, optimizer=self.optimizer)
+        self.create_swa_scheduler(self.optimizer, self.model)
 
     def create_optimizer(self):
         """
@@ -54,6 +75,8 @@ class Trainer_lr_decay(Trainer):
         We provide a reasonable default that works well. If you want to use something else, you can pass a tuple in the
         Trainer's init through `optimizers`, or subclass and override this method in a subclass.
         """
+        lr_decay_rate = 1 if self.lr_decay_rate == -1 else self.lr_decay_rate
+
         opt_model = self.model
 
         if self.optimizer is None:
@@ -76,7 +99,7 @@ class Trainer_lr_decay(Trainer):
                 cur_name = name.split('.')[0]
 
                 if cur_name != prev_name:
-                    lr *= self.lr_decay_rate
+                    lr *= lr_decay_rate
                 prev_name = cur_name
                 
                 if name in decay_parameters:
@@ -98,6 +121,61 @@ class Trainer_lr_decay(Trainer):
                     params=optimizer_grouped_parameters,
                     **optimizer_kwargs)
         return self.optimizer
+
+    def create_swa_scheduler(self, optimizer: torch.optim.Optimizer = None, model : torch.nn.Module = None):
+        """ 创建stochatic weight average scheduler
+        """
+        if self.swa and self.swa_scheduler is None:
+            self.swa_model = AveragedModel(model)
+            self.swa_scheduler = SWALR(optimizer, swa_lr=0.05)
+
+
+    def create_swa_scheduler(self, optimizer: torch.optim.Optimizer = None, model : torch.nn.Module = None):
+        """ 创建stochatic weight average scheduler
+        """
+        if self.swa and self.swa_scheduler is None:
+            self.swa_model = AveragedModel(model)
+            self.swa_scheduler = SWALR(optimizer, swa_lr=0.05)
+
+
+    def train_swa(self):
+        if not self.swa:
+            return self.model
+
+        train_dataloader = self.get_train_dataloader()
+
+        args = self.args
+
+        tr_loss = torch.tensor(0.0).to(args.device)
+
+        for epoch in range(self.swa_update_epoches):
+            if isinstance(train_dataloader, DataLoader) and isinstance(train_dataloader.sampler, DistributedSampler):
+                train_dataloader.sampler.set_epoch(epoch)
+            elif isinstance(train_dataloader.dataset, IterableDataset):
+                train_dataloader.dataset.set_epoch(epoch)
+            self.optimizer.zero_grad()
+
+            for step, input in enumerate(train_dataloader):
+                loss = self.compute_loss(self.model, input)
+                loss.backward()
+                tr_loss += loss.detach()
+
+                if (step + 1) % args.gradient_accumulation_steps == 0:
+                    self.optimizer.step()
+                    self.swa_model.update_parameters(self.model)
+                    self.swa_scheduler.step()
+                    self.optimizer.zero_grad()
+
+        # Update bn statistics for the swa_model at the end
+        torch.optim.swa_utils.update_bn(train_dataloader, self.swa_model)
+        # Use swa_model to make predictions on test data 
+        return self.swa_model
+
+    def set_model_as_swa_model(self):
+        """将模型设置为参数平均后的模型，方便测试
+        """
+        self.model = self.swa_model
+
 
 # def create_lr_decay_optimizer(trainer, lr_decay):
 #     layer_names = []
