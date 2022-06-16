88b5f17816add5d462332a851defa2dcbc7566b3
Merealtea <67770232+Merealtea@users.noreply.github.com>
2022-06-16 21:25:23 +0800

    update why part

--- src/GP_dataloader.py
+++ src/GP_dataloader.py
@@ -255,6 +255,127 @@ class CollateForEnt_Chaizi:
 
     def __call__(self, examples):
         return self.collate(examples=examples)
+
+class CollateForEnt_fenci:
+    def __init__(self, tokenizer, max_length):
+        self.tokenizer = tokenizer
+        self.max_length = max_length
+
+    def encoder(self, item):
+        text = item[0]
+        token2char_span_mapping = self.tokenizer(text, return_offsets_mapping=True, max_length=self.max_length, truncation=True)["offset_mapping"]
+        start_mapping = {j[0]: i for i, j in enumerate(token2char_span_mapping) if j != (0, 0)}
+        end_mapping = {j[-1] - 1: i for i, j in enumerate(token2char_span_mapping) if j != (0, 0)}
+        #将raw_text的下标 与 token的start和end下标对应
+        encoder_txt = self.tokenizer.encode_plus(text, max_length=self.max_length, truncation=True)
+        input_ids = encoder_txt["input_ids"]
+        token_type_ids = encoder_txt["token_type_ids"]
+        attention_mask = encoder_txt["attention_mask"]
+
+        return text, start_mapping, end_mapping, input_ids, token_type_ids, attention_mask
+
+    def sequence_padding(self, inputs, length=None, value=0, seq_dims=1, mode='post'):
+        """Numpy函数，将序列padding到同一长度
+        """
+        if length is None:
+            length = np.max([np.shape(x)[:seq_dims] for x in inputs], axis=0)
+        elif not hasattr(length, '__getitem__'):
+            length = [length]
+
+        slices = [np.s_[:length[i]] for i in range(seq_dims)]
+        slices = tuple(slices) if len(slices) > 1 else slices[0]
+        pad_width = [(0, 0) for _ in np.shape(inputs[0])]
+
+        outputs = []
+        for x in inputs:
+            x = x[slices]
+            for i in range(seq_dims):
+                if mode == 'post':
+                    pad_width[i] = (0, length[i] - np.shape(x)[i])
+                elif mode == 'pre':
+                    pad_width[i] = (length[i] - np.shape(x)[i], 0)
+                else:
+                    raise ValueError('"mode" argument must be "post" or "pre".')
+            x = np.pad(x, pad_width, 'constant', constant_values=value)
+            outputs.append(x)
+
+        return np.array(outputs)
+
+    def align_token(self, fenci_idx, start_mapping, end_mapping):
+        new_idx = [0]
+        for i in range(len(fenci_idx)):
+            if i not in start_mapping:
+                continue
+
+            new_idx.append(fenci_idx[i])
+        new_idx.append(0)
+        return new_idx
+
+    def collate(self, examples):
+        raw_text_list, batch_input_ids, batch_attention_mask, batch_labels, batch_segment_ids = [], [], [], [], []
+        batch_fenci_idx = []
+        for item in examples:
+            raw_text, start_mapping, end_mapping, input_ids, token_type_ids, attention_mask = self.encoder(item)
+            fenci_idx = self.align_token(item[1],start_mapping,end_mapping)
+            labels = np.zeros((len(EE_label2id), self.max_length, self.max_length))
+            for start, end, label in item[2:]:
+                if start in start_mapping and end in end_mapping:
+                    start = start_mapping[start]
+                    end = end_mapping[end]
+                    labels[label, start, end] = 1
+            raw_text_list.append(raw_text)
+            batch_input_ids.append(input_ids)
+            batch_segment_ids.append(token_type_ids)
+            batch_attention_mask.append(attention_mask)
+            batch_labels.append(labels[:, :len(input_ids), :len(input_ids)])
+            batch_fenci_idx.append(fenci_idx)
+        batch_inputids = torch.tensor(self.sequence_padding(batch_input_ids)).long()
+        batch_segmentids = torch.tensor(self.sequence_padding(batch_segment_ids)).long()
+        batch_attentionmask = torch.tensor(self.sequence_padding(batch_attention_mask)).float()
+        batch_labels = torch.tensor(self.sequence_padding(batch_labels, seq_dims=3)).long()
+        batch_labels_ = torch.zeros((batch_labels.shape[0],batch_labels.shape[1],1))
+        batch_fenci_idx = torch.tensor(self.sequence_padding(batch_fenci_idx)).long()
+        inputs = {
+            "input_ids":batch_inputids,
+            "attention_mask":batch_attentionmask,
+            "token_type_ids":batch_segmentids,
+            "labels": batch_labels,
+            "labels_": batch_labels_,
+            "fenci_ids": batch_fenci_idx
+        }
+        return inputs
+
+    def __call__(self, examples):
+        return self.collate(examples=examples)
+
+
+def load_data_fenci(path):
+    D = []
+    for d in json.load(open(path)):
+        D.append([d['text']])
+        
+        seg_list = jieba.cut(d['text'], cut_all=False) 
+        cut_char_idx = np.zeros((len(d['text'])))
+        cnt = 0
+        for word in seg_list:
+            if len(word) == 1:
+                cut_char_idx[cnt] = 0 #S
+                cnt += 1
+            else:
+                cut_char_idx[cnt] = 1 #B
+                cnt += 1
+                for i in range(1, len(word)-1):
+                    cut_char_idx[cnt] = 2 #M
+                    cnt += 1
+                cut_char_idx[cnt] = 3 #E
+                cnt += 1
+        D[-1].append(cut_char_idx)
+
+        for e in d['entities']:
+            start, end, label = e['start_idx'], e['end_idx'], e['type']
+            if start <= end:
+                D[-1].append((start, end, EE_label2id[label]))
+    return D
     
 
 if __name__ == '__main__':
