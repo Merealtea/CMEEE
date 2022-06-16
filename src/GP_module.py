445e164ada240f14d2d169c056699ae80fe533a9
YHWmz <64775174+YHWmz@users.noreply.github.com>
2022-06-16 22:43:41 +0800

    Update GP_module.py

--- src/GP_module.py
+++ src/GP_module.py
@@ -305,3 +305,152 @@ class GlobalPointer_Chaizi(nn.Module):
         
         trainer_results = self._get_trainer_results(logits)
         return NEROutputs(loss, trainer_results)
+ 
+
+
+class GlobalPointer_fenci(nn.Module):
+    def __init__(self, encoder, ent_type_size, inner_dim, fenci_embsz=32, RoPE=True):
+        #encodr: RoBerta-Large as encoder
+        #inner_dim: 64
+        #ent_type_size: ent_cls_num
+        super().__init__()
+        self.encoder = encoder
+        self.ent_type_size = ent_type_size
+        self.inner_dim = inner_dim
+        self.hidden_size = encoder.config.hidden_size
+        self.dense = nn.Linear(self.hidden_size + fenci_embsz, self.ent_type_size * self.inner_dim * 2)
+        self.fenci_embedding = nn.Embedding(4, fenci_embsz)
+        self.fenci_embsz = fenci_embsz
+
+        self.RoPE = RoPE
+
+    def sinusoidal_position_embedding(self, batch_size, seq_len, output_dim):
+        position_ids = torch.arange(0, seq_len, dtype=torch.float).unsqueeze(-1)
+
+        indices = torch.arange(0, output_dim // 2, dtype=torch.float)
+        indices = torch.pow(10000, -2 * indices / output_dim)
+        embeddings = position_ids * indices
+        embeddings = torch.stack([torch.sin(embeddings), torch.cos(embeddings)], dim=-1)
+        embeddings = embeddings.repeat((batch_size, *([1]*len(embeddings.shape))))
+        embeddings = torch.reshape(embeddings, (batch_size, seq_len, output_dim))
+        embeddings = embeddings.to(self.device)
+        return embeddings
+
+    def _pred_labels(self, _logits):
+        return torch.argmax(_logits, dim=1)
+
+    def _get_trainer_results(self, _logits):
+        tmp = _logits.reshape(_logits.shape[0],_logits.shape[1],-1).permute(0,2,1)
+        return tmp[:,:,:1]
+
+    def get_pred(self, input_ids, attention_mask, token_type_ids, fenci_ids):
+        self.device = input_ids.device
+        
+        context_outputs = self.encoder(input_ids, attention_mask, token_type_ids)
+        # last_hidden_state:(batch_size, seq_len, hidden_size)
+        last_hidden_state = context_outputs[0]
+        chaizi_output = self.fenci_forward(fenci_ids=fenci_ids)
+        # chaizi_output:Batch_size X Seq_len X 48
+
+        last_hidden_state = torch.cat((last_hidden_state,chaizi_output),dim = -1)
+
+        batch_size = last_hidden_state.size()[0]
+        seq_len = last_hidden_state.size()[1]
+
+        # outputs:(batch_size, seq_len, ent_type_size*inner_dim*2)
+        outputs = self.dense(last_hidden_state)
+        outputs = torch.split(outputs, self.inner_dim * 2, dim=-1)
+        # outputs:(batch_size, seq_len, ent_type_size, inner_dim*2)
+        outputs = torch.stack(outputs, dim=-2)
+        # qw,kw:(batch_size, seq_len, ent_type_size, inner_dim)
+        qw, kw = outputs[...,:self.inner_dim], outputs[...,self.inner_dim:]
+        if self.RoPE:
+            # pos_emb:(batch_size, seq_len, inner_dim)
+            pos_emb = self.sinusoidal_position_embedding(batch_size, seq_len, self.inner_dim)
+            # cos_pos,sin_pos: (batch_size, seq_len, 1, inner_dim)
+            cos_pos = pos_emb[..., None, 1::2].repeat_interleave(2, dim=-1)
+            sin_pos = pos_emb[..., None,::2].repeat_interleave(2, dim=-1)
+            qw2 = torch.stack([-qw[..., 1::2], qw[...,::2]], -1)
+            qw2 = qw2.reshape(qw.shape)
+            qw = qw * cos_pos + qw2 * sin_pos
+            kw2 = torch.stack([-kw[..., 1::2], kw[...,::2]], -1)
+            kw2 = kw2.reshape(kw.shape)
+            kw = kw * cos_pos + kw2 * sin_pos
+        # logits:(batch_size, ent_type_size, seq_len, seq_len)
+        logits = torch.einsum('bmhd,bnhd->bhmn', qw, kw)
+
+        # padding mask
+        pad_mask = attention_mask.unsqueeze(1).unsqueeze(1).expand(batch_size, self.ent_type_size, seq_len, seq_len)
+        logits = logits*pad_mask - (1-pad_mask)*1e12
+
+        # 排除下三角
+        mask = torch.tril(torch.ones_like(logits), -1)
+        logits = logits - mask * 1e12
+
+        logits = logits/self.inner_dim**0.5
+        
+        return logits
+
+    def fenci_forward(self, fenci_ids):
+        """
+        chaizi_ids: Batch_size X Seq_len
+        output: Batch_size X Seq_len X 48
+        """
+        output_shape = [fenci_ids.shape[0],fenci_ids.shape[1],self.fenci_embsz]
+        output = self.fenci_embedding(fenci_ids)
+        # print(output.shape)
+        return output
+
+    def forward(self, input_ids, attention_mask, token_type_ids, fenci_ids, labels = None, labels_ = None):
+        self.device = input_ids.device
+        
+        context_outputs = self.encoder(input_ids, attention_mask, token_type_ids)
+        # last_hidden_state:(batch_size, seq_len, hidden_size)
+        last_hidden_state = context_outputs[0]
+        chaizi_output = self.fenci_forward(fenci_ids=fenci_ids)
+        # chaizi_output:Batch_size X Seq_len X 48
+
+        last_hidden_state = torch.cat((last_hidden_state,chaizi_output),dim = -1)
+
+        batch_size = last_hidden_state.size()[0]
+        seq_len = last_hidden_state.size()[1]
+
+        # outputs:(batch_size, seq_len, ent_type_size*inner_dim*2)
+        outputs = self.dense(last_hidden_state)
+        outputs = torch.split(outputs, self.inner_dim * 2, dim=-1)
+        # outputs:(batch_size, seq_len, ent_type_size, inner_dim*2)
+        outputs = torch.stack(outputs, dim=-2)
+        # qw,kw:(batch_size, seq_len, ent_type_size, inner_dim)
+        qw, kw = outputs[...,:self.inner_dim], outputs[...,self.inner_dim:]
+        if self.RoPE:
+            # pos_emb:(batch_size, seq_len, inner_dim)
+            pos_emb = self.sinusoidal_position_embedding(batch_size, seq_len, self.inner_dim)
+            # cos_pos,sin_pos: (batch_size, seq_len, 1, inner_dim)
+            cos_pos = pos_emb[..., None, 1::2].repeat_interleave(2, dim=-1)
+            sin_pos = pos_emb[..., None,::2].repeat_interleave(2, dim=-1)
+            qw2 = torch.stack([-qw[..., 1::2], qw[...,::2]], -1)
+            qw2 = qw2.reshape(qw.shape)
+            qw = qw * cos_pos + qw2 * sin_pos
+            kw2 = torch.stack([-kw[..., 1::2], kw[...,::2]], -1)
+            kw2 = kw2.reshape(kw.shape)
+            kw = kw * cos_pos + kw2 * sin_pos
+        # logits:(batch_size, ent_type_size, seq_len, seq_len)
+        logits = torch.einsum('bmhd,bnhd->bhmn', qw, kw)
+
+        # padding mask
+        pad_mask = attention_mask.unsqueeze(1).unsqueeze(1).expand(batch_size, self.ent_type_size, seq_len, seq_len)
+        logits = logits*pad_mask - (1-pad_mask)*1e12
+
+        # 排除下三角
+        mask = torch.tril(torch.ones_like(logits), -1)
+        logits = logits - mask * 1e12
+
+        logits = logits/self.inner_dim**0.5
+        if labels == None:
+            loss = None
+        else: 
+            loss = loss_fun(logits, labels)
+        
+        trainer_results = self._get_trainer_results(logits)
+        return NEROutputs(loss, trainer_results)
+
